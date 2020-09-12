3e95d7bce7ddd8d047f8054c6bf2d95cdc57076b
corey-marchand <corey.marchand@me.com>
2020-09-11 21:44:42 -0700

    added selectors, checkout page, and modified styling in checkout dropdown box

--- src/App.js
+++ src/App.js
@@ -2,23 +2,17 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header-component/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
-
+import { selectCurrentUser } from './redux/user/user.selectors';
+import { createStructuredSelector } from 'reselect';
 import './App.css';
 
 class App extends React.Component{
-//   constructor(){
-//     super();
-
-//     this.state = {
-//       currentUser: null
-//     }
-//   }
-
   unsubscribeFromAuth = null
 
   componentDidMount(){
@@ -50,6 +44,7 @@ class App extends React.Component{
            <Switch>
               <Route exact path='/' component={HomePage} />
               <Route path='/shop' component={ShopPage} />
+              <Route exact path='/checkout' component={CheckoutPage} />
               <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUp />)}/>
             </Switch>
         </div>
@@ -57,8 +52,8 @@ class App extends React.Component{
     );
   }
 }
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 // dispatch is a way to tell redux that whatever your passing is an action object that will be passed to every reducer
